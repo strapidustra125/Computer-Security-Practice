85f700db5499dd280f6ed19abdd890fca7df938f
StrapiDuStra <strapidustra125@gmail.com>
2020-09-28 20:26:49 +0400

    Полностью написал сигнатурный поиск

---
+++ Signature_Search/Signature_Search/Signature_Search.cpp
@@ -0,0 +1,246 @@
+﻿#include <fstream>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+#define MSG_SUCCESS 10
+#define MSG_ERROR	11
+
+wstring directoryPath;
+
+int signatureStartPosition = 0;
+
+//char signature[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+char signature[16];
+string signatureString = "";
+
+vector<wstring> paths;
+vector<wstring> result;
+
+bool IsNotDotName(std::wstring const& Filename)
+{
+	return ((Filename != L".") && (Filename != L".."));
+}
+
+bool IsDirectory(DWORD FileAttrs)
+{
+	return ((0 != (FileAttrs & FILE_ATTRIBUTE_DIRECTORY)) && (0 == (FileAttrs & FILE_ATTRIBUTE_REPARSE_POINT)));
+}
+
+void EnumerateFileObjects(std::wstring const& Path)
+{
+	using namespace std;
+
+	WIN32_FIND_DATAW FindData;
+	wstring const Mask = Path + L"\\*";
+	HANDLE  const hFind = FindFirstFileW(Mask.c_str(), &FindData);
+
+	if (INVALID_HANDLE_VALUE != hFind)
+	{
+		do
+		{
+			if (IsNotDotName(&FindData.cFileName[0]))
+			{
+				wstring const FullPath = Path + L"\\" + &FindData.cFileName[0];
+				
+				if (FindData.dwFileAttributes != FILE_ATTRIBUTE_DIRECTORY)
+				{
+					paths.push_back(FullPath);
+				}
+
+				if (IsDirectory(FindData.dwFileAttributes))
+				{
+					EnumerateFileObjects(FullPath);
+				}
+			}
+		} while (NULL != FindNextFileW(hFind, &FindData));
+
+		FindClose(hFind);
+	}
+}
+
+void printVector(vector<wstring> vec)
+{
+	for (vector<wstring>::iterator it = vec.begin(); it < vec.end(); it++)
+	{
+		wcout << '\t' << *it << endl;
+	}
+}
+
+
+bool signIsFound(wstring path)
+{
+	string temp = "", file = "";
+	bool result;
+	char newSymb;
+
+	ifstream in(path, ios::binary);
+
+	while (1)
+	{
+		newSymb = in.get();
+		if (in.eof()) break;
+		file += newSymb;
+	}
+
+	if (file.find(signatureString) == std::string::npos) result = false;
+	else result = true;
+
+	in.close();
+	return result;
+}
+
+int readSignature()
+{
+	bool unsuccessReading = true;
+	string signFilePath = "";
+	ifstream inSign;
+	char newSymb;
+	char signatureCounter = 0;
+	int symbolCounter = 0;
+	int startSignaturePointer = 0;
+	
+	cout << "Enter name of file in current directory or full path to the file to read signature." << endl;
+	cout << "For example: C:\\files\\signature.txt" << endl;
+
+newPathLabel:
+	cout << "Path to signature file: ";
+
+	cin >> signFilePath;
+	cout << signFilePath << endl;
+
+	inSign.open(signFilePath, ios::binary);
+
+	while (!inSign.is_open())
+	{
+		cout << "ERROR: Sorry, It's unable to open the file..." << endl;
+		cout << "Please, try again: ";
+		cin >> signFilePath;
+		cout << signFilePath << endl;
+		inSign.open(signFilePath, ios::binary);
+	}
+
+	cout << "Signature file was successfully opened!" << endl << endl;
+
+	cout << "Enter start position in file to read the signature: ";
+	cin >> startSignaturePointer;
+	
+	while (unsuccessReading)
+	{
+		while (1)
+		{
+			newSymb = inSign.get();
+			if (inSign.eof()) break;
+
+			if (symbolCounter >= startSignaturePointer)
+			{
+				if (signatureCounter < 16)
+				{
+					signature[signatureCounter] = newSymb;
+					signatureCounter++;
+				}
+				else break;
+			}
+			symbolCounter++;
+		}
+
+		inSign.close();
+
+		if (signatureCounter < 16)
+		{
+			signatureCounter = 0;
+			symbolCounter = 0;
+
+			cout << "ERROR: Too short file to read the signature :(" << endl;
+
+			if (startSignaturePointer != 0)
+			{
+				cout << "Start position may be less value." << endl;
+				cout << "Please, try another position: ";
+
+				cin >> startSignaturePointer;
+
+				inSign.open(signFilePath, ios::binary);
+			}
+			else
+			{
+				cout << "Please, try another file." << endl;
+				signFilePath = "";
+				goto newPathLabel;
+			}
+		}
+		else unsuccessReading = false;
+	}
+
+	cout << endl;
+	return MSG_SUCCESS;
+}
+
+void printSignature()
+{
+	cout << endl << "Signature: ";
+	for (char i = 0; i < 16; i++)
+	{
+		cout << signature[i];
+	}
+	cout << endl;
+}
+
+void createSignatureString()
+{
+	for (int i = 0; i < 16; i++) signatureString += signature[i];
+}
+int main(int argc, char* argv[])
+{
+	cout << endl << "================ Start Signature Search program ================" << endl << endl;
+
+	if (readSignature() == MSG_SUCCESS)
+	{
+		cout << "Successful readiong of signature!" << endl;
+		printSignature();
+	}
+
+	createSignatureString();
+	
+	cout << endl << "Enter path to directory for Signature Search." << endl;
+	cout << "Directory: ";
+	wcin >> directoryPath;
+	
+	EnumerateFileObjects(directoryPath);
+
+	while (paths.empty())
+	{
+		cout << "ERROR: There are NO files in this directory!" << endl;
+		cout << "Please, try again: ";
+		wcin >> directoryPath;
+		EnumerateFileObjects(directoryPath);
+	}
+	
+	cout << endl << "The list of the file pathes:" << endl;
+	printVector(paths);
+	
+	for (vector<wstring>::iterator it = paths.begin(); it < paths.end(); it++)
+	{
+		if (signIsFound(*it))
+		{
+			result.push_back(*it);
+		}
+	}
+
+	if (result.empty()) cout << "There are NO files includes signature!" << endl;
+	else
+	{
+		cout << endl << "Signature was found in:" << endl;
+		printVector(result);
+	}
+	
+	cout << endl << "================ Finish Signature Search program ===============" << endl << endl;
+
+	system("pause");
+
+	return 0;
+}
---
+++ Steganography_1_encrypt/Steganography_1_encrypt/Steganography_1_encrypt.cpp
@@ -0,0 +1,202 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
+ifstream in("In.txt");
+ofstream out("Out.txt");
+
+bool error = false;
+
+string revString(string s)
+{
+	string temp = "";
+	for (int i = s.length() - 1; i >= 0; i--)
+	{
+		temp += s[i];
+	}
+	return temp;
+}
+
+vector<string> deleteEndSpaces(vector<string> file)
+{
+	vector<string> newVec;
+	for (vector<string>::iterator i = file.begin(); i < file.end(); i++)
+	{
+		string temp = "";
+		for (int j = (*i).length() - 1; j >= 0; j--)
+		{
+			if ((*i)[j] != ' ')
+			{
+				for (int k = j; k >= 0; k--)
+				{
+					temp += (*i)[k];
+				}
+				j = 0;
+			}
+		}
+		temp = revString(temp);
+		newVec.push_back(temp);
+	}
+	return newVec;
+}
+
+vector<string> readFile()
+{
+	vector<string> file;
+	string s;
+	while (!in.eof())
+	{
+		getline(in, s);
+		file.push_back(s);
+	}
+	return file;
+}
+
+string charToBinString(char c)
+{
+	string s = "";
+	int code = c,
+		temp;
+
+	while (code != 0)
+	{
+		temp = code % 2;
+		code = code / 2;
+
+		if (temp == 1) s += '1';
+		else if (temp == 0) s += '0';
+	}
+
+	if (s.length() < 8)
+	{
+		for (int i = s.length(); i < 8; i++)
+		{
+			s += '0';
+		}
+	}
+	s = revString(s);
+
+	return s;
+}
+
+vector<string> addCode(string code, vector<string> file)
+{
+	vector<string>::iterator it = file.begin();
+	string temp;
+
+	if (code.length() * 8 > file.size())
+	{
+		out << "Длинна шифра больше количества строк файла..." << code.length() << "    " << code.length() * 8 << "    " << file.size() << endl;
+		error = true;
+	}
+	else
+	{
+		for (int i = 0; i < code.length(); i++)
+		{
+			temp = charToBinString(code[i]);
+			for (int j = 0; j < 8; j++)
+			{
+				if (temp[j] == '1') (*it) += ' ';
+				it++;
+			}
+		}
+	}
+	return file;
+}
+
+char binStrToChar(string s)
+{
+	int code = 0,
+		temp = 0;
+
+	for (int i = s.length() - 1, j = 0; i >= 0; i--, j++)
+	{
+		if (s[i] == '1') temp = 1;
+		else if (s[i] == '0') temp = 0;
+
+		code += temp * pow(2, j);
+	}
+	return (char)code;
+}
+
+string readCode(vector<string> file)
+{
+	string code = "",
+		temp = "";
+
+	for (vector<string>::iterator i = file.begin(); i < file.end(); i++)
+	{
+		if ((*i) == "") temp += '0';
+		else
+		{
+			if ((*i)[(*i).length() - 1] == ' ') temp += '1';
+			else temp += '0';
+		}
+		if (temp.length() == 8)
+		{
+			code += binStrToChar(temp);
+			temp = "";
+		}
+
+	}
+
+	return code;
+}
+
+int main()
+{
+
+	system("pause");
+
+	cout << endl << "================ Start Steganography_1 program ================" << endl << endl;
+
+	vector<string> file;
+	string sIn;
+	string code = "";
+
+
+
+	cout << "Enter message to encrypt." << endl;
+	cout << "Message: ";
+	getline(cin, code);
+
+	cout << "\'" << code << "\'" << endl;
+
+	system("pause");
+
+
+
+
+
+	/*
+
+	in >> sIn;
+	if (sIn == "write")
+	{
+		getline(in, code);
+		code.erase(0, 1);
+		file = readFile();
+		file = deleteEndSpaces(file);
+		file = addCode(code, file);
+
+		if (!error) for (vector<string>::iterator i = file.begin(); i < file.end(); i++) out << *i << endl;
+	}
+	else if (sIn == "read")
+	{
+		getline(in, code);
+		file = readFile();
+		out << readCode(file) << endl;
+	}
+	*/
+
+	cout << endl << "================ Finish Steganography_1 program ===============" << endl << endl;
+
+	system("pause");
+
+	return 0;
+}
+
